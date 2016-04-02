cb34aa6248e50d26668db3379a9a72735df59dd4
tsingwong <919916459@qq.com>
2016-04-02 18:57:20 +0800

    更新AI部分

--- js/script.js
+++ js/script.js
@@ -1,12 +1,78 @@
 var chessBoard = [];
 var me = true;
+var over = false;
 
+// 胜利数组
+var wins = [];
+
+// 胜利统计数组
+var myWin = [];
+var computerWin = [];
+
+// 初始化棋盘
 for (var i = 0; i < 15; i++) {
     chessBoard[i] = [];
     for (var j = 0; j < 15; j++) {
         chessBoard[i][j] = 0;
     }
 }
+
+//初始化胜利数组
+for (var i = 0; i < 15; i++) {
+    wins[i] = [];
+    for (var j = 0; j < 15; j++) {
+        wins[i][j] = [];
+    }
+}
+
+var count = 0;
+
+// 横向五子胜利数组
+for (var i = 0; i < 15; i++) {
+    for (var j = 0; j < 11; j++) {
+        for (var k = 0; k < 5; k++) {
+            wins[i][j + k][count] = true;
+        }
+        count++;
+    }
+}
+
+// 竖向五子胜利数组
+for (var i = 0; i < 15; i++) {
+    for (var j = 0; j < 11; j++) {
+        for (var k = 0; k < 5; k++) {
+            wins[j + k][i][count] = true;
+        }
+        count++;
+    }
+}
+
+// 正斜向五子胜利数组
+for (var i = 0; i < 11; i++) {
+    for (var j = 0; j < 11; j++) {
+        for (var k = 0; k < 5; k++) {
+            wins[i + k][j + k][count] = true;
+        }
+        count++;
+    }
+}
+
+// 反斜向五子胜利数组
+for (var i = 0; i < 11; i++) {
+    for (var j = 14; j > 3; j--) {
+        for (var k = 0; k < 5; k++) {
+            wins[i + k][j - k][count] = true;
+        }
+        count++;
+    }
+}
+
+// 初始化两种分数
+for (var i = 0; i < count; i++) {
+    myWin[i] = 0;
+    computerWin[i] = 0;
+}
+
 
 var chess = document.getElementById('chess');
 var context = chess.getContext('2d');
@@ -52,18 +118,123 @@ var oneStep = function(i, j, me) {
     context.fill();
 }
 
+// 人点击落子事件
 chess.onclick = function(e) {
+    if (over) {
+        return;
+    }
+    if (!me) {
+        return;
+    }
     var x = e.offsetX;
     var y = e.offsetY;
     var i = Math.floor(x / 30);
     var j = Math.floor(y / 30);
+    // 限制只能落子在空位上
     if (chessBoard[i][j] == 0) {
         oneStep(i, j, me);
-        if (me) {
-        	chessBoard[i][j] = 1;
-        }else{
-        	chessBoard[i][j] = 2;
+        chessBoard[i][j] = 1;
+        for (var k = 0; k < count; k++) {
+            if (wins[i][j][k]) {
+                myWin[k]++;
+                computerWin[k] = 6; //不可能在这种赢法上取胜
+                // 判断人落子后是否胜利
+                if (myWin[k] == 5) {
+                    window.alert("你赢了");
+                    over = true;
+                }
+            }
         }
-        me = !me;
+        // 判断是否胜利，否则交换落子权
+        if (!over) {
+            me != me;
+            computerAI();
+        }
+    }
+}
+
+var computerAI = function() {
+    var myScore = [];
+    var computerScore = [];
+    var max = 0;
+    var u = 0,
+        v = 0;
+
+    for (var i = 0; i < 15; i++) {
+        myScore[i] = [];
+        computerScore[i] = [];
+        for (var j = 0; j < 15; j++) {
+            myScore[i][j] = 0;
+            computerScore[i][j] = 0;
+        }
+    }
+    for (var i = 0; i < 15; i++) {
+        for (var j = 0; j < 15; j++) {
+            if (chessBoard[i][j] == 0) {
+                for (var k = 0; k < count; k++) {
+                    if (wins[i][j][k]) {
+                        // 计算机拦截机制
+                        if (myWin[k] == 1) {
+                            myScore[i][j] += 200;
+                        } else if (myWin[k] == 2) {
+                            myScore[i][j] += 400;
+                        } else if (myWin[k] == 3) {
+                            myScore[i][j] += 2000;
+                        } else if (myWin[k] == 4) {
+                            myScore[i][j] += 10000;
+                        }
+                        // 计算机胜利机制
+                        if (computerWin[k] == 1) {
+                            computerScore[i][j] += 220;
+                        } else if (computerWin[k] == 2) {
+                            computerScore[i][j] += 420;
+                        } else if (computerWin[k] == 3) {
+                            computerScore[i][j] += 2200;
+                        } else if (computerWin[k] == 4) {
+                            computerScore[i][j] += 20000;
+                        }
+                    }
+                }
+                // 拦截机制中最高分数的点
+                if (myScore[i][j] > max) {
+                    max = myScore[i][j];
+                    u = i;
+                    v = j;
+                } else if (myScore[i][j] == max) {
+                    if (computerScore[i][j] > computerScore[u][v]) {
+                        u = i;
+                        v = j;
+                    }
+                }
+                // 计算机胜利机制中最高分数的点
+                if (computerScore[i][j] > max) {
+                    max = computerScore[i][j];
+                    u = i;
+                    v = j;
+                } else if (computerScore[i][j] == max) {
+                    if (myScore[i][j] > myScore[u][v]) {
+                        u = i;
+                        v = j;
+                    }
+                }
+            }
+        }
+    }
+    oneStep(u, v, false);
+    chessBoard[u][v] = 2;
+    for (var k = 0; k < count; k++) {
+        if (wins[u][v][k]) {
+            computerWin[k]++;
+            myWin[k] = 6; //不可能在这种赢法上取胜
+            // 判断计算机落子后是否胜利
+            if (computerWin[k] == 5) {
+                window.alert("计算机赢了");
+                over = true;
+            }
+        }
+    }
+    // 判断是否胜利，否则交换落子权
+    if (!over) {
+        me != me;
     }
 }
